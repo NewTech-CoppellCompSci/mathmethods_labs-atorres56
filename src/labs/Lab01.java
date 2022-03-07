cb25ea8893be57532eed2c7829631ef425ebe243
atorres56 <89420335+atorres56@users.noreply.github.com>
2022-03-07 17:29:22 -0600

    Math Labs

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -16,8 +16,19 @@ public class Lab01 {
 
 	
 	public static void problem01() {
+		//asks user for a positive integer
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		double num = inKey.nextInt();
 		
+		//asks user for another positive integer
+		System.out.print("Enter a positive integer: ");
+		double num2 = inKey.nextInt();
 		
+		//double newNum finds the sum of the num to the power of num 2
+		double newNum = Math.pow(num, num2);
+		//prints out the new number (num to the power of num2)
+		System.out.println(num + "^" + num2 + " = " + newNum);
 		
 	}
 	
@@ -25,7 +36,16 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
+		//asks user for a positive integer
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		double num = inKey.nextInt();
 		
+		//holds the square root of the number the user inputed
+		double sqrNum = Math.sqrt(num);
+		
+		//prints out the square root
+		System.out.println("The square root of " + num + " is " + sqrNum);
 		
 		
 	}
@@ -35,18 +55,53 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		//asks user for the one side of the triangle
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Length of side A: ");
+		double sideA = inKey.nextInt();
+				
 		
+		//asks user for the other side of the triangle
+		System.out.print("Length of side B: ");
+		double sideB = inKey.nextInt();
+		
+		//finds the sum of both sides to the power of 2
+		double aSqr = Math.pow(sideA, 2);
+		double bSqr = Math.pow(sideB, 2);
+		
+		//adds the a^2 plus the c^2
+		double add = aSqr + bSqr;
+		//finds the square root of "add"
+		double hypot = Math.sqrt(add);
+		
+		//prints out hypotenuse
+		System.out.println("Hypotenuse: " + hypot);
 		
 
 	}
 
-
 	
 	
 	public static void problem04() {
+	Scanner inKey = new Scanner(System.in);
 		
-		
-		
+	int max = Integer.MIN_VALUE;	
+	int min = Integer.MAX_VALUE;
+	//int input has a value of -1
+	int input = -1;
+	//runs while input does not = 0
+	while(input != 0) {
+		//asks user for a integer
+		System.out.println("Enter an integer: ");
+		input = inKey.nextInt();
+		//finds the max and the min of all the numbers
+		max = Math.max(max, input);
+		min = Math.min(min, input);
+	}
+	//prints out the max and the min
+	System.out.println("Max = " + max);
+	System.out.println("Min = " + min);
+	
 	}
 	
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -16,19 +16,53 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		//asks user for a positive integer
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		int num = inKey.nextInt();
+		//asks user for another positive integer			
+		System.out.print("Enter another positive integer: ");
+		int num2 = inKey.nextInt();
 		
+		//finds the max and the min between the two numbers
+		int max = Math.max(num, num2);
+		int min = Math.min(num, num2);
 		
+		//creates the scope
+		int scope = max - min;
 		
-		
+		//for loop runs 10 times
+		for(int i = 1; i < 10; i++) {
+			//finds a random number between the two numbers given
+			int rand = (int) (Math.random() * scope + min);
+			//prints out the random numbers
+			System.out.print(rand + " ");
+
+		}
+		System.out.println();
 	}
 	
 	
 	
 	
 	public static void problem02() {
+		//asks user for the radius
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter the radius: ");
+		double radius = inKey.nextInt();
 		
+		//asks user for the height
+		System.out.print("Enter the height: ");
+		double height = inKey.nextInt();
 		
+		double radSqr = Math.pow(radius, 2);
 		
+		double volume = Math.PI * radSqr * height;
+	
+		System.out.println("The volume is: " + volume);
+	
+	
+	
 	}
 
 	
